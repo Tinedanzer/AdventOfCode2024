dabb0b0108af17f09f1cea13949e2a5d1fba380b
TINEDANZER\johnh <johnhillallen@gmail.com>
2024-12-08 16:26:12 -0600

    day 1 of advent of coding

---
+++ Day1/part1/ElfOnTheShelf.py
@@ -0,0 +1,23 @@
+from get_input import get_input
+
+input_data = get_input("input.txt")
+
+test = str(input_data).split()
+
+bucket1 = []
+bucket2 = []
+# grooming data while transforming strings into ints
+for line in input_data:
+    bucket1.append(int(line[0:5]))
+    bucket2.append(int(line[-5:]))
+
+bucket1.sort()
+bucket2.sort()
+
+total_distance = 0
+# finding the distance between two numbers and totalling
+for x, y in zip(bucket1,bucket2):
+    total_distance += abs(y-x)
+    # print(x,y)
+    
+print(total_distance)
